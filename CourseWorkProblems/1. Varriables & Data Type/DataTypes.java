c64601c364a2156a5799e9a3645cc68c472ef3b8
KartikUgale <kartikugale822@gmain.com>
2025-05-17 12:36:26 +0530

    add Variables and Data Types Folder with some codes

---
+++ CourseWorkProblems/1. Varriables & Data Type/DataTypes.java
@@ -0,0 +1,32 @@
+public class DataTypes{
+    public static void Main(String args[]){
+
+        // Data Types in Java --> Premetive & Non-Premetive
+        // there are all 8 Premetive data types in java
+
+        byte n = 8;                 // 8 bit Integer                -128 to 127
+        System.out.println(n);
+
+        short s = 7867;             // 16 bit Integer               -32,768 to 32,767
+        System.out.println(s);
+
+        char a = 'b';               // 16 bit Unicode Character     e.g- 'a', '9', '@'
+        System.out.println(a);
+
+        int i = 90;                 // 32 bit Integer               -2^31 to 2^31-1
+        System.out.println(i);
+
+        float f = 89.7f;            // 32 bit floating Point        e.g- 1.2f, -2.3f
+        System.out.println(f);
+
+        long l = 6574563;           // 64 bit Integer               -2^63 to 2^63-1
+        System.out.println(l);
+
+        double d = 8976;            // 64 bit floating Point        e.g- 1.289889, -2.3
+        System.out.println(d);
+
+        boolean t = false;          // 1 byte (Logical : true or False)
+        System.out.println(t);
+
+    }
+}
---
+++ CourseWorkProblems/1. Varriables & Data Type/Q3.java
@@ -0,0 +1,26 @@
+// Simple Bill
+
+import java.util.*;
+public class Q3{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter Prices \nPencil = ");
+        float Pencil = sc.nextFloat();
+
+        System.out.print("Pen = "); 
+        float Pen = sc.nextFloat(); 
+
+        System.out.print("Eraser = "); 
+        float Eraser = sc.nextFloat(); System.out.println();
+
+        System.out.println("-----Your Bill-----");
+        System.out.println("Pencil : " + Pencil);
+        System.out.println("Pen : "+ Pen);
+        System.out.println("Eraser : "+ Eraser);
+
+        Float Total = Pencil+Pen+Eraser;
+        System.out.println("\n Total : "+ Total);
+
+    }
+}
