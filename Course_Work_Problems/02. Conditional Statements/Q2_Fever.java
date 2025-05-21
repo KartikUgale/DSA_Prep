0bb7b6b0065a6bf9a34f9272b1c744a8f3c37c67
KartikUgale <kartikugale822@gmain.com>
2025-05-21 20:23:28 +0530

    update: Lexicographic title

---
+++ Course_Work_Problems/02. Conditional Statements/Q2_Fever.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Q2_Fever {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your Temperature in F");
+        float YourTemp = sc.nextFloat();
+
+        if (YourTemp >= 100) {
+            System.out.println("You have a fever");
+        } else {
+            System.out.println("You dont have a fever");
+        }
+
+        sc.close();
+    }
+}
